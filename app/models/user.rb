5ca905f2a675ab99a24066712e9868dbd99757b3
Willian Gustavo Veiga <contact@willianveiga.com>
2015-01-31 14:01:10 -0200

    valid_credentials? method created to check if an user can log in.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,25 @@
-require 'securerandom'
 require 'bcrypt'
 
 class User < ActiveRecord::Base
   validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :password, presence: true, length: { minimum: 8 }, confirmation: true
   belongs_to :company
-  before_save :generate_salt, :hash_password
+  before_create :generate_salt, :hash_password
+
+  def self.valid_credentials?(username, password)
+    user = self.find_by_username username
+    return false unless user
+    salted_password = BCrypt::Engine.hash_secret(password, user.salt)
+    salted_password == user.password
+  end
 
   private
   def generate_salt
-    self.salt = SecureRandom.base64
+    self.salt = BCrypt::Engine.generate_salt
   end
 
   def hash_password
-    salted_password = BCrypt::Password.create(self.salt << self.password)
+    salted_password = BCrypt::Engine.hash_secret(self.password, self.salt)
     self.password = salted_password
   end
 end
