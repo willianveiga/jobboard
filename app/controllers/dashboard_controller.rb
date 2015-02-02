76699a5a659c82a8ea99a2ceb697e1a272dcc40f
Willian Gustavo Veiga <contact@willianveiga.com>
2015-02-02 21:33:27 -0200

    Job list created for dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   before_action :redirect_if_user_not_logged_in, :index
+  before_action :set_user, :index
+  layout 'dashboard'
 
   def index
   end
@@ -7,5 +9,10 @@ class DashboardController < ApplicationController
   private
   def redirect_if_user_not_logged_in
     redirect_to users_login_path unless session[:user_id]
+  end
+
+  def set_user
+    @user = User.find(session[:user_id])
+    @company = @user.company
   end
 end
