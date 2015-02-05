c0c30b79a22de47163fe3e296927df133e220eed
Willian Gustavo Veiga <contact@willianveiga.com>
2015-02-04 22:25:18 -0200

    Job model is full text searchable

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,21 @@
 class Job < ActiveRecord::Base
   belongs_to :company
 
+  searchable do
+    text :title, :description
+    text :company do
+      company.name if company
+    end
+  end
+
   def self.find_all_active
     where(active: true)
+  end
+
+  def self.search_all(criteria)
+    query = search do
+      fulltext criteria
+    end
+    query.results
   end
 end
