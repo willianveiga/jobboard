2ec5cdaba8ba0482d31a3edce7423d32cecc6993
Willian Gustavo Veiga <contact@willianveiga.com>
2015-02-07 20:39:18 -0200

    Nasty hack. The full text search should return all active jobs where the related company is active too. Now, the results provided by the full text search are just active jobs. I have not found a way to include a "where clause" like to filter by active companies. To bypass this I have created a private method that filter the results. It's bad but it could be worst (SQL "LIKE" I am looking at you!).

--- app/models/job.rb
+++ app/models/job.rb
@@ -14,12 +14,23 @@ class Job < ActiveRecord::Base
       fulltext criteria
       with :active, true
     end
-    query.results
+    self.results_where_company_is_active query.results
   end
 
   def self.find_all_active
     select('jobs.title, jobs.brief_description, jobs.company_id')
       .joins(:company)
       .where('companies.active = ? AND jobs.active = ?', true, true)
+  end
+
+  private
+  def self.results_where_company_is_active(results)
+    filtered_results = []
+    results.each do |job|
+      if job.company.active
+        filtered_results << job
+      end
+    end
+    filtered_results
   end
 end
