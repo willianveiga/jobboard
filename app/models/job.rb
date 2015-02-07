4745f0dea17bd5067294301d43ea0fb7ee70fc4a
Willian Gustavo Veiga <contact@willianveiga.com>
2015-02-07 19:17:18 -0200

    Jobs must be active to be displayed in the search results page.

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,21 +3,23 @@ class Job < ActiveRecord::Base
 
   searchable do
     text :title, :brief_description, :description
+    boolean :active
     text :company do
-      company.name if company
+      company.name
     end
+  end
+
+  def self.search_all(criteria)
+    query = search do
+      fulltext criteria
+      with :active, true
+    end
+    query.results
   end
 
   def self.find_all_active
     select('jobs.title, jobs.brief_description, jobs.company_id')
       .joins(:company)
       .where('companies.active = ? AND jobs.active = ?', true, true)
-  end
-
-  def self.search_all(criteria)
-    query = search do
-      fulltext criteria
-    end
-    query.results
   end
 end
