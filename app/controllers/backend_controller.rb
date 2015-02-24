a0f63e644f13b09ae670aa33c7af212b29249525
Willian Gustavo Veiga <contact@willianveiga.com>
2015-02-24 07:46:06 -0300

    A filter is more appropriate for the redirect.

--- app/controllers/backend_controller.rb
+++ app/controllers/backend_controller.rb
@@ -1,5 +1,11 @@
 class BackendController < ApplicationController
+  before_filter :redirect_if_user_not_logged_in
+
   def index
+  end
+
+  private
+  def redirect_if_user_not_logged_in
     redirect_to signin_path unless session[:user_id]
   end
 end
